714f777628333b5263d752a9ab1b158515d3949d
Dimas Saifullah Ramadhan <37479804+dimasRamadhan2398@users.noreply.github.com>
2025-06-09 09:32:09 +0700

    Update app_hukum.py

--- app_hukum.py
+++ app_hukum.py
@@ -8,12 +8,11 @@ kalimat_perkara = st.text_input("Masukkan perkara")
 # Inisialisasi session_state jika belum ada
 cari = st.button("Cari Peraturan & Putusan")
 peraturan, putusan_ma, putusan_mk = st.tabs(["Peraturan", "Putusan MA", "Putusan MK"])  
-if cari and kalimat_perkara:
-    with peraturan:
-        hasil_peraturan(kalimat_perkara)
+with peraturan:
+    hasil_peraturan(kalimat_perkara)
 
-    with putusan_ma:
-        hasil_putusan_ma(kalimat_perkara)
+with putusan_ma:
+    hasil_putusan_ma(kalimat_perkara)
 
-    with putusan_mk:
-        hasil_putusan_mk(kalimat_perkara)
+with putusan_mk:
+    hasil_putusan_mk(kalimat_perkara)
